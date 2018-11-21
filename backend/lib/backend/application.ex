06c761d4e2df5e4da822ca7c874a1f6ebe4e77e1
Jan Christophersen <jan@ruken.pw>
2018-11-21 16:41:54 +0100

    Cache API hot paths

--- backend/lib/backend/application.ex
+++ backend/lib/backend/application.ex
@@ -14,7 +14,8 @@ defmodule Backend.Application do
       supervisor(BackendWeb.Endpoint, []),
       # Start your own worker by calling: Backend.Worker.start_link(arg1, arg2, arg3)
       # worker(Backend.Worker, [arg1, arg2, arg3]),
-      worker(Backend.Wows.BackgroundRefresh, [])
+      worker(Backend.Wows.BackgroundRefresh, []),
+      {ConCache, [name: :ships_cache, ttl_check_interval: false]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
