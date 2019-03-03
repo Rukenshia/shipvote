aee92fd4cc64cd1ba01fb95f6aff17b1e3603055
Jan Christophersen <jan@chrstphrsn.cc>
2019-03-03 12:17:36 +0100

    Fix previous commit

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -20,7 +20,7 @@ config :backend, BackendWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :warn
+config :logger, level: :info
 
 # ## SSL Support
 #
