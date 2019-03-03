499aada5ecba8211133beda1bf6d71f5bdfc9b25
Jan Christophersen <jan@chrstphrsn.cc>
2019-03-03 12:40:46 +0100

    Enable background deletion

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -20,7 +20,7 @@ config :backend, BackendWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
 # ## SSL Support
 #
