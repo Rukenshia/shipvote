4935a20d9c4256f58e97ad51b30773b40229a730
Jan Christophersen <jan@ruken.pw>
2019-03-05 11:48:37 +0100

    Run npm build in deploy step

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -6,5 +6,6 @@ cd /root/shipvote/backend
 git pull
 mix deps.get && \
 mix ecto.migrate && \
+cd assets && npm ci && npm run deploy && cd .. && \
 mix phx.digest && \
 sudo systemctl restart shipvote
