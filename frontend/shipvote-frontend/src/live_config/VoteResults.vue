495195eb574833b649aba20ce24c2994f6cd2071
Jan Christophersen <jan@ruken.pw>
2019-03-06 16:00:57 +0100

    Add relative time to the frontend of VoteResults

--- frontend/shipvote-frontend/src/live_config/VoteResults.vue
+++ frontend/shipvote-frontend/src/live_config/VoteResults.vue
@@ -1,8 +1,8 @@
 <template>
   <mdc-list two-line bordered>
-    <mdc-list-item v-for="vote in votes.slice(0, 3)" :key="vote.id">
+    <mdc-list-item v-for="vote in votes.slice(votes.length - 3, votes.length)" :key="vote.id">
       <span>X ship</span>
-      <span slot="secondary">x minutes ago</span>
+      <span slot="secondary">{{relativeTime(vote.updated_at)}}</span>
     </mdc-list-item>
   </mdc-list>
 </template>
@@ -12,6 +12,37 @@ export default {
   props: ['votes'],
   data() {
     return {};
+  },
+  methods: {
+    relativeTime(since) {
+
+      var msPerMinute = 60 * 1000;
+      var msPerHour = msPerMinute * 60;
+      var msPerDay = msPerHour * 24;
+      var msPerMonth = msPerDay * 30;
+      var msPerYear = msPerDay * 365;
+
+      var elapsed = new Date() - new Date(since);
+
+      if (elapsed < msPerMinute) {
+        const rounded = Math.round(elapsed/1000);
+        return `${rounded} second${rounded !== 1 ? 's' : ''} ago`;
+      }
+
+      else if (elapsed < msPerHour) {
+        const rounded = Math.round(elapsed/msPerMinute);
+        return `${rounded} minute${rounded !== 1 ? 's' : ''} ago`;
+      }
+
+      else if (elapsed < msPerDay ) {
+        const rounded = Math.round(elapsed/msPerHour);
+        return `${rounded} hour${rounded !== 1 ? 's' : ''} ago`;
+      }
+
+      else {
+        return 'some days ago';
+      }
+    }
   }
 }
 </script>
