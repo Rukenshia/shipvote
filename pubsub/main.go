4e38079381c58ef0b0eac9c0dc41a13dd01d75de
Jan Christophersen <jan@chrstphrsn.cc>
2019-06-14 16:54:29 +0200

    Allow CORS in API [backend skip]

--- pubsub/main.go
+++ pubsub/main.go
@@ -13,13 +13,14 @@ func main() {
 	controller.AddCurrentVotes()
 
 	e := echo.New()
-	e.POST("/api/channels/:channel_id/votes", controller.OpenVote)
-	e.PATCH("/api/channels/:channel_id/votes/:vote_id/status", controller.CloseVote)
-	e.POST("/api/channels/:channel_id/votes/:vote_id/submit", controller.VoteForShip)
-	e.Logger.Fatal(e.Start(":1323"))
-
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*.ext-twitch.tv", "localhost.rig.twitch.tv"},
 		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
 	}))
+
+	e.POST("/api/channels/:channel_id/votes", controller.OpenVote)
+	e.PATCH("/api/channels/:channel_id/votes/:vote_id/status", controller.CloseVote)
+	e.POST("/api/channels/:channel_id/votes/:vote_id/submit", controller.VoteForShip)
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
