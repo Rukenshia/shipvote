96631813d842ab851b20e20b2c7c84843784148a
Jan Christophersen <jan@ruken.pw>
2019-06-22 12:42:35 +0200

    Use correct parameter for cacert file, deploy with ASG launch [api skip]

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -21,12 +21,20 @@ aws ec2 create-launch-template-version --launch-template-name "${LAUNCH_TEMPLATE
 # change default version to the new one
 aws ec2 modify-launch-template --launch-template-name "${LAUNCH_TEMPLATE_NAME}" --default-version '$Latest' > /dev/null
 
+# increase instance count
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name "${SCALING_GROUP_NAME}" --max-size 2 --desired-capacity 2
+
+# wait for the scaling
+sleep 300
+
+# increase instance count
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name "${SCALING_GROUP_NAME}" --max-size 1 --desired-capacity 1
+
 # destroy current instance
-instances="$(aws ec2 describe-instances --filters Name=tag:app,Values=shipvote Name=tag:tier,Values=web | jq -r '.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId')" 
+# instances="$(aws ec2 describe-instances --filters Name=tag:app,Values=shipvote Name=tag:tier,Values=web | jq -r '.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId')" 
 
+# for instance in $instances; do
+# 	echo "Terminating instance ${instance}"
 
-for instance in $instances; do
-	echo "Terminating instance ${instance}"
-
-	aws ec2 terminate-instances --instance-ids "${instance}"
-done
+# 	aws ec2 terminate-instances --instance-ids "${instance}"
+# done
