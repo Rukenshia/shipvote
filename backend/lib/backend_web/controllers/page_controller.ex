052b636194358335ae0a8e45f6b3c09cc30cb684
Jan Christophersen <code@chrstphrsn.cc>
2020-03-24 22:10:27 +0100

    [api skip] Add loaderio verification

--- backend/lib/backend_web/controllers/page_controller.ex
+++ backend/lib/backend_web/controllers/page_controller.ex
@@ -8,4 +8,8 @@ defmodule BackendWeb.PageController do
   def health(conn, _params) do
     text(conn, "#{Application.spec(:backend, :vsn)}_aws")
   end
+
+  def loaderio(conn, _params) do
+    text(conn, "loaderio-ab47bee856c880d39a24bce59211bcfd")
+  end
 end
