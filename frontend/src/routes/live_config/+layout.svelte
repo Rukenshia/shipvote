fa09ee1f1a545fd1e8ac2ad52c1d37bb2f825454
Jan Christophersen <code@chrstphrsn.cc>
2023-10-03 14:17:01 +0200

    remove old frontend

--- frontend/src/routes/live_config/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="bg-gray-900 min-h-screen p-4">
-  <slot />
-</div>
--- frontend/tailwind.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.svelte'],
-  plugins: [require('@tailwindcss/forms')]
-};
