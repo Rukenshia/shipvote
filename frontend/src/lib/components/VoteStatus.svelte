f146708a5f5ab18d6f44760b222074bffdbe6634
Jan Christophersen <code@chrstphrsn.cc>
2024-08-31 11:20:54 +0200

    fix remaining vote time

--- frontend/src/lib/components/VoteStatus.svelte
+++ frontend/src/lib/components/VoteStatus.svelte
@@ -27,31 +27,28 @@
 
 {#if $vote && $vote.status === "open"}
   <Box>
-    The vote is currently
+    <div class="flex items-center justify-around">
+      <h2 class="text-2xl font-semibold"></h2>
+    </div>
+    <div class="mt-2 flex flex-col justify-around gap-2 px-4">
+      <button
+        on:click={() => closeVote()}
+        class="rounded-md px-8 py-4 font-medium transition active:bg-cyan-600 active:ring-2 active:ring-cyan-400"
+        class:text-gray-800={$vote !== undefined}
+        class:text-gray-200={$vote === undefined}
+        class:bg-gray-700={$vote === undefined}
+        class:bg-cyan-500={$vote !== undefined}
+        class:hover:bg-cyan-400={$vote !== undefined}
+        disabled={$vote === undefined}>Close vote</button
+      >
 
-    <span class="font-medium"> open </span>
-
-    {#if $vote && $vote.status === "open"}
-      <div class="mt-2 flex flex-col justify-around gap-2 px-4">
-        <button
-          on:click={() => closeVote()}
-          class="rounded-md px-8 py-4 font-medium transition active:bg-cyan-600 active:ring-2 active:ring-cyan-400"
-          class:text-gray-800={$vote !== undefined}
-          class:text-gray-200={$vote === undefined}
-          class:bg-gray-700={$vote === undefined}
-          class:bg-cyan-500={$vote !== undefined}
-          class:hover:bg-cyan-400={$vote !== undefined}
-          disabled={$vote === undefined}>Close vote</button
-        >
-
-        {#if vote.ends_at}
-          <RemainingVoteTime
-            started_at={$vote.created_at}
-            ends_at={$vote.ends_at}
-          />
-        {/if}
-      </div>
-    {/if}
+      {#if $vote.ends_at}
+        <RemainingVoteTime
+          started_at={$vote.created_at}
+          ends_at={$vote.ends_at}
+        />
+      {/if}
+    </div>
 
     <div>
       {#if $vote && $vote.status === "open"}
